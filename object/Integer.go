f2c2b063afd84e4aa5924e8d46226a78f0fc4f5c
maojun <maojun@didiglobal.com>
2023-08-17 22:28:33 +0800

    Integer

---
+++ object/Integer.go
@@ -0,0 +1,16 @@
+package object
+
+import "fmt"
+
+// Integer 整型表示
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
--- object/object.go
+++ object/object.go
@@ -3,6 +3,10 @@ package object
 // ObjectType 值在go的表示形式 对象类型
 type ObjectType string
 
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
 // Object 对象接口 每个不同类型的值都有自己的表示对象形式
 type Object interface {
 	Type() ObjectType
